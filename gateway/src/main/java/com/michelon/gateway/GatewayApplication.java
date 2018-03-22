fb5b4b213fc4c75f3a4324c0ba1078d126bd9e21
Maicon Michelon <maicon.f.michelon@gmail.com>
2018-03-22 14:25:41 -0300

    zuul funcionando

--- gateway/src/main/java/com/michelon/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/michelon/gateway/GatewayApplication.java
@@ -3,12 +3,23 @@ package com.michelon.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+	@GetMapping("/info")
+	public ResponseEntity<String> info() {
+		return ResponseEntity.ok("Zuul server");
+	}
+
+
 }
